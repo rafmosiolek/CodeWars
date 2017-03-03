2ec0bdb04d626faf969be2e781e53483e28f3cbb
RafM <rkjmosiolek@gmail.com>
2017-03-03 12:28:04 +0100

    disemvowel trolls challenge added

---
+++ disemvowel-trolls-challenge.js
@@ -0,0 +1,24 @@
+/*
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+*/
+
+
+
+function disemvowel(str) {
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    var solution = "";
+    for (i=0; i<=str.length; i++) {
+        if (vowels.indexOf(str.charAt(i)) === -1) {
+            solution = solution + str.charAt(i);
+        }
+    }
+    return solution;
+}
+
+disemvowel("This website is for losers LOL!"); // Output: 'Ths wbst s fr lsrs LL!'
