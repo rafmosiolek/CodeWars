db8fa29166431ff19b5ae1ca60894f91459954d8
RafM <rkjmosiolek@gmail.com>
2017-04-28 23:56:54 +0200

    grassopper summation solution

--- grasshopperSummation.js
+++ grasshopperSummation.js
@@ -3,3 +3,7 @@ Summation
 Write a program that finds the summation of every number between 1 and num. 
 The number will always be a positive integer greater than 0.
 */
+
+function summation1(num) {
+  return num * (num + 1) / 2;
+}
